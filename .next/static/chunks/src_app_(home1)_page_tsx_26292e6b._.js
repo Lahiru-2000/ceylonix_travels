b1442fd9857b7346cc32c64b93c4cfa210f11b54
Lahiru Jayarathne <99554634+Lahiru-2000@users.noreply.github.com>
2026-04-05 10:10:44 +0530

    updated vercel errors

--- .next/static/chunks/src_app_(home1)_page_tsx_26292e6b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_Components_6d31bafd._.js",
-  "static/chunks/node_modules_3594ca25._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_(home1)_page_tsx_977d1322._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_Components_6d31bafd._.js",
-  "static/chunks/node_modules_e8c706d3._.js"
-],
-    source: "dynamic"
-});
